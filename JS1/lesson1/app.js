6eed82dcf2b02084e8f863f698456d8daf443c24
Stefan <olegvoronin953@gmail.com>
2021-09-22 12:18:37 +0500

    homework №3

--- JS1/lesson1/app.js
+++ JS1/lesson1/app.js
@@ -1,21 +1,136 @@
 'use strict'
 
-// 1
-let Tc = prompt('Введите тепературу');
-let Tf = (9 / 5) * Tc + 32;
-alert (Tf);
-
-// 2
-let name = 'Василий'
-let admin = name;
-console.log(admin);
-
-// 3
-
-/* 
-1. 10 + 10 будет 20 и прибваив '10' мы получили '2010' произошло приведение типов
-2. 10 + '10' будет '1010' произошло приведение типов после мы опять прибавили 10 и получили '101010'
-3. 10 + 10 будет 20 и прибваив +'10' мы получили 30, т.к  +  перед '10' превратил значаение string в number
-4. унарный минус делает число отрицательным
-5. получилось NaN, во первых числа пишутся без "", во вторых числа с десятичными, сотыми и тысячными должны ограничиваться  . (2.5)   
+/**
+ * если число четное то оно подпишется, если оно нечетное то оно тоже поджпишется, нуль так же подпишется  
+ * @param {number} i счетчик до 10 включительно, начинас с 0 прибавляя по 1 
 */
+
+
+// Пример 1
+for (let i = 0; i <= 10; i++) {
+    if(i == 0) {
+        console.log(' - это ноль');
+    } else if (i % 2 == 0) {
+        console.log(i + ' - четное число');
+    } else {
+        console.log(i + ' - нечетное число');
+    } 
+    
+}
+// почему нуль не выводится в консоль??
+
+
+// Пример 2
+
+const post = {
+    author: "John", //вывести этот текст
+    postId: 23,
+    comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+                likes: 10,
+                dislikes: 2 //вывести это число
+            }
+        },
+        {
+            userId: 5, //вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", //вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1
+            }   
+        },
+    ]
+};
+
+console.log(post.author); 
+console.log(post.comments[0].rating.dislikes);
+console.log(post.comments[1].userId);
+console.log(post.comments[1].text);
+
+// Прмер 3
+/**
+ * с помощью forEach перебираем массив, находим цену и добавляем скидку каждому товару
+ */
+
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(function(discount) {
+    return discount.price = discount.price - discount.price / 100 * 15;
+})
+
+console.log(products);
+
+// Пример 4
+
+/**
+ * Функция получает все товары с фотографиями с помощью filter, затем сорирует товары по цене с помощью sort
+ * @param {number}
+ */
+
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+        "1.jpg",
+        "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+        "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+];
+
+let searchPhotos = products.filter(function(element) {
+    return element.photos && element.photos.length >= 1;
+});
+
+let upPrice = products.sort(function(elem1,  elem2){
+    return elem1.price - elem2.price;
+});
+
+console.log(upPrice);
+console.log(searchPhotos);
+
+
+// Пример 6
+
+for(let x = 0; x <= 20; x++) {
+    let str = "";
+    for(let y = 0; y <= x; y++) {
+        str += "*";
+    }
+    console.log(str);
+}
